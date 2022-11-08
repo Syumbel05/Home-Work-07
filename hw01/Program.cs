fdaad6ad7b9ce4e2dfb58f9a92b19df7aeec243d
syumbel <syu.kashapova@mail.ru>
2022-11-08 15:24:58 +0500

    добавила коды в файл

---
+++ hw01/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+double [,] matrix=new double[3,4];
+
+void PrintArray(double[,]matrix)
+{
+for (int i=0;i<3;i++)
+    {
+for (int j=0;j<4;j++)
+      {
+    Console.Write($"{matrix[i,j]}");
+      }
+Console.WriteLine();
+    }
+}
+void FillArray(double[,]matrix)
+{
+for (int i=0;i<matrix.GetLength(0);i++)
+  {
+    for (int j=0;j<matrix.GetLength(1);j++)
+    {
+
+matrix[i,j]=Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+    }
+  }
+}
+FillArray(matrix);
+PrintArray(matrix);
